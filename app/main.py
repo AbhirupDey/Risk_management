37b3593336dcfccade38feaeb4d2922fe8a9e56f
AbhirupDey <93918523+AbhirupDey@users.noreply.github.com>
2025-04-13 22:59:40 +0530

    Delete app directory

--- app/main.py
+++
@@ -1,5 +0,0 @@
-# Redirects to the unified application file
-from app.main_unified import *
-
-# This file exists to maintain backward compatibility
-# All functionality has been merged into main_unified.py
